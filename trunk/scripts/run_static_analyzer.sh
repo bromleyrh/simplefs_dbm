d812e08c7c6e3272bd4c3ad6bbe5650d0cff2c4a
Richard Bromley <bromleyrh@gmail.com>
2019-02-23 13:33:19 -0800

    Add run_static_analyzer.sh script
    Commit UUID: d9e993a0-e977-4f25-8c33-6cbdeeafd397

---
+++ trunk/scripts/run_static_analyzer.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+dummy_file=scan_build
+
+configure_opts="--enable-debugging"
+
+rm_cache_file()
+{
+	if [ $cache_file != /dev/null ]; then
+		rm -fv $cache_file
+	fi
+}
+
+do_configure()
+{
+	rm_cache_file
+
+	scan-build ./configure $configure_opts
+	touch $dummy_file
+}
+
+set -e
+
+# import Autoconf cache file path variable
+cache_file=/dev/null
+for f in /usr/share/config.site /usr/local/share/config.site; do
+	if [ -r $f ]; then
+		. $f
+	fi
+done
+
+if [ ! -f configure ]; then
+	echo "\"configure\" script not found (must run from root build directory)" \
+		1>&2
+	exit 1
+fi
+
+trap rm_cache_file EXIT INT TERM HUP
+
+if [ ! -f Makefile ]; then
+	do_configure
+elif [ ! -f $dummy_file ]; then
+	echo "Project not configured for static analysis" 1>&2
+	make distclean
+	do_configure
+fi
+
+scan-build make $@
+
+# vi: set noexpandtab sw=4 ts=4:
