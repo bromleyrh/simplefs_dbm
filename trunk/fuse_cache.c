4a03c670d1b92edad8244afc6b14182eb1bf2d93
Richard Bromley <bromleyrh@gmail.com>
2020-04-10 21:52:46 -0700

    Commit initial work in fuse_cache (1)
    Commit UUID: 3f76250e-55cf-4601-bd01-a92b23bd8467

---
+++ trunk/fuse_cache.c
@@ -0,0 +1,7 @@
+/*
+ * fuse_cache.c
+ */
+
+#include "fuse_cache.h"
+
+/* vi: set expandtab sw=4 ts=4: */
---
+++ trunk/fuse_cache.h
@@ -0,0 +1,10 @@
+/*
+ * fuse_cache.h
+ */
+
+#ifndef _FUSE_CACHE_H
+#define _FUSE_CACHE_H
+
+#endif
+
+/* vi: set expandtab sw=4 ts=4: */
