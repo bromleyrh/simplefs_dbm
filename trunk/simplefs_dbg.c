60cee2bf8e2c5111fd8146d712fbe36e5801cc81
Richard Bromley <bromleyrh@gmail.com>
2020-06-15 15:25:05 -0700

    Commit initial work in simplefs_dbg (2)
    Commit UUID: 0c804448-d674-4cbe-ada7-06229c4f9c75

--- trunk/simplefs_dbg.c
+++ trunk/simplefs_dbg.c
@@ -18,6 +18,8 @@
 #include <readline/readline.h>
 
 #include <errno.h>
+#include <inttypes.h>
+#include <signal.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -30,10 +32,33 @@ enum op {
     OP_FIX_UP = 1
 };
 
-static int parse_cmdline(int, char **, enum op *, const char **);
+static volatile sig_atomic_t quit;
+
+#define OBJSZ(obj) sizeof(struct db_obj_##obj)
+
+static void print_usage(const char *);
+static int parse_cmdline(int, char **, enum op *, const char **, int *);
+
+static void int_handler(int);
 
 static int uint64_cmp(uint64_t, uint64_t);
 static int db_key_cmp(const void *, const void *, void *);
+
+static void disp_header(FILE *, const struct db_key *, const void *, size_t);
+static void disp_free_ino(FILE *, const struct db_key *, const void *, size_t);
+static void disp_dirent(FILE *, const struct db_key *, const void *, size_t);
+static void disp_stat(FILE *, const struct db_key *, const void *, size_t);
+static void disp_page(FILE *, const struct db_key *, const void *, size_t);
+static void disp_xattr(FILE *, const struct db_key *, const void *, size_t);
+static void disp_ulinked_inode(FILE *, const struct db_key *, const void *,
+                               size_t);
+
+static int dump_db_obj(FILE *, const void *, const void *, size_t, const char *,
+                       void *);
+
+static int dump_cb(const void *, const void *, size_t, void *);
+
+static int dump_db(FILE *, struct dbh *);
 
 static int cmd_dump(struct dbh *);
 static int cmd_find(struct dbh *);
@@ -44,7 +69,7 @@ static int cmd_update(struct dbh *);
 
 static int process_cmd(struct dbh *);
 
-static int do_fix_up_interactive(const char *);
+static int do_fix_up_interactive(const char *, int);
 
 static void
 print_usage(const char *prognm)
@@ -53,15 +78,18 @@ print_usage(const char *prognm)
            "\n"
            "    -F PATH use specified database file path\n"
            "    -f      fix up file system metadata\n"
-           "    -h      output help\n",
+           "    -h      output help\n"
+           "    -w      open database file for writing (default is to open for "
+           "reading only)\n",
            prognm);
 }
 
 static int
-parse_cmdline(int argc, char **argv, enum op *op, const char **db_pathname)
+parse_cmdline(int argc, char **argv, enum op *op, const char **db_pathname,
+              int *ro)
 {
     for (;;) {
-        int opt = getopt(argc, argv, "F:fh");
+        int opt = getopt(argc, argv, "F:fhw");
 
         if (opt == -1)
             break;
@@ -76,12 +104,23 @@ parse_cmdline(int argc, char **argv, enum op *op, const char **db_pathname)
         case 'h':
             print_usage(argv[0]);
             return -2;
+        case 'w':
+            *ro = 0;
+            break;
         default:
             return -1;
         }
     }
 
     return 0;
+}
+
+static void
+int_handler(int signum)
+{
+    (void)signum;
+
+    quit = 1;
 }
 
 static int
@@ -125,12 +164,220 @@ db_key_cmp(const void *k1, const void *k2, void *key_ctx)
     return cmp;
 }
 
+static void
+disp_header(FILE *f, const struct db_key *key, const void *data,
+            size_t datasize)
+{
+    struct db_obj_header *hdr = (struct db_obj_header *)data;
+
+    (void)key;
+    (void)datasize;
+
+    fprintf(f, "I-node count %" PRIu64, hdr->numinodes);
+}
+
+static void
+disp_free_ino(FILE *f, const struct db_key *key, const void *data,
+              size_t datasize)
+{
+    (void)data;
+    (void)datasize;
+
+    fprintf(f, "number %" PRIu64 " to %" PRIu64,
+            (uint64_t)(key->ino), (uint64_t)(key->ino) + FREE_INO_RANGE_SZ - 1);
+}
+
+static void
+disp_dirent(FILE *f, const struct db_key *key, const void *data,
+            size_t datasize)
+{
+    struct db_obj_dirent *de = (struct db_obj_dirent *)data;
+
+    (void)datasize;
+
+    fprintf(f, "directory %" PRIu64 ", name %s -> node %" PRIu64,
+            (uint64_t)(key->ino), key->name, (uint64_t)(de->ino));
+}
+
+static void
+disp_stat(FILE *f, const struct db_key *key, const void *data, size_t datasize)
+{
+    struct db_obj_stat *s = (struct db_obj_stat *)data;
+
+    (void)datasize;
+
+    fprintf(f, "node %" PRIu64 " -> st_ino %" PRIu64,
+            (uint64_t)(key->ino), (uint64_t)(s->st_ino));
+}
+
+static void
+disp_page(FILE *f, const struct db_key *key, const void *data, size_t datasize)
+{
+    (void)data;
+
+    fprintf(f, "node %" PRIu64 ", page %" PRIu64 ", size %zu",
+            (uint64_t)(key->ino), (uint64_t)(key->pgno), datasize);
+}
+
+static void
+disp_xattr(FILE *f, const struct db_key *key, const void *data, size_t datasize)
+{
+    (void)data;
+
+    fprintf(f, "node %" PRIu64 ", name %s, size %zu",
+            (uint64_t)(key->ino), key->name, datasize);
+}
+
+static void
+disp_ulinked_inode(FILE *f, const struct db_key *key, const void *data,
+                   size_t datasize)
+{
+    (void)data;
+    (void)datasize;
+
+    fprintf(f, "node %" PRIu64,
+            (uint64_t)(key->ino));
+}
+
+static int
+dump_db_obj(FILE *f, const void *key, const void *data, size_t datasize,
+            const char *prefix, void *ctx)
+{
+    struct db_key *k = (struct db_key *)key;
+
+    static const struct {
+        const char  *dispstr;
+        size_t      datasize;
+        void        (*datadisp)(FILE *, const struct db_key *, const void *,
+                                size_t);
+    } objinfo[] = {
+        [TYPE_HEADER]           = {"Header",
+                                   OBJSZ(header),  &disp_header},
+        [TYPE_FREE_INO]         = {"Free I-node number information",
+                                   0,              &disp_free_ino},
+        [TYPE_DIRENT]           = {"Directory entry",
+                                   OBJSZ(dirent),  &disp_dirent},
+        [TYPE_STAT]             = {"I-node entry",
+                                   OBJSZ(stat),    &disp_stat},
+        [TYPE_PAGE]             = {"Page",
+                                   0,              &disp_page},
+        [TYPE_XATTR]            = {"Extended attribute entry",
+                                   0,              &disp_xattr},
+        [TYPE_ULINKED_INODE]    = {"Unlinked I-node entry",
+                                   0,              &disp_ulinked_inode}
+    }, *objinfop;
+
+    (void)ctx;
+
+    if (k->type >= ARRAY_SIZE(objinfo))
+        goto type_err;
+
+    objinfop = &objinfo[k->type];
+    if (objinfop->datadisp == NULL)
+        goto type_err;
+
+    if ((objinfop->datasize != 0) && (datasize != objinfop->datasize)) {
+        fprintf(stderr, "%s data size %zu incorrect\n", objinfop->dispstr,
+                datasize);
+        return -EILSEQ;
+    }
+
+    fprintf(f, "%s%s: ", prefix, objinfop->dispstr);
+    (*(objinfop->datadisp))(f, k, data, datasize);
+    fputc('\n', f);
+
+    return 0;
+
+type_err:
+    fprintf(stderr, "Invalid object type %d\n", k->type);
+    return -EILSEQ;
+}
+
+static int
+dump_cb(const void *key, const void *data, size_t datasize, void *ctx)
+{
+    if (quit)
+        return 1;
+
+    return dump_db_obj((FILE *)ctx, key, data, datasize, "", NULL);
+}
+
+static int
+dump_db(FILE *f, struct dbh *dbh)
+{
+    return db_hl_walk(dbh, &dump_cb, f);
+}
+
 static int
 cmd_dump(struct dbh *dbh)
 {
-    (void)dbh;
+    char *path;
+    FILE *f;
+    int err;
+    struct sigaction oldsa;
+
+    static const struct sigaction sa = {
+        .sa_handler = &int_handler
+    };
+
+    path = readline("Output file name: ");
+    if (path == NULL)
+        return 0;
+
+    f = fopen(path, "w");
+    if (f == NULL) {
+        err = -errno;
+        error(0, -err, "Error opening %s", path);
+        goto err1;
+    }
+
+    if (sigaction(SIGINT, &sa, &oldsa) == -1) {
+        err = -errno;
+        goto err2;
+    }
+
+    fputs("Dumping...", stderr);
+
+    err = dump_db(f, dbh);
+    fputc('\n', stderr);
+    if (err) {
+        if (err == 1) {
+            fputs("Dump interrupted\n", stderr);
+            err = 2;
+        }
+        goto err3;
+    }
+
+    if (sigaction(SIGINT, &oldsa, NULL) == -1) {
+        err = -errno;
+        goto err2;
+    }
+
+    if ((fflush(f) != 0) || (fsync(fileno(f)) == -1)) {
+        err = -errno;
+        error(0, -err, "Error writing %s", path);
+        goto err2;
+    }
+
+    if (fclose(f) != 0) {
+        err = -errno;
+        error(0, -err, "Error closing %s", path);
+        goto err1;
+    }
+
+    free(path);
+
+    fputs("Dump file written\n", stderr);
 
     return 0;
+
+err3:
+    sigaction(SIGINT, &oldsa, NULL);
+err2:
+    fclose(f);
+err1:
+    free(path);
+    return err;
 }
 
 static int
@@ -236,13 +483,13 @@ input_err:
 }
 
 static int
-do_fix_up_interactive(const char *db_pathname)
+do_fix_up_interactive(const char *db_pathname, int ro)
 {
     int ret;
     struct dbh *dbh;
 
     ret = db_hl_open(&dbh, db_pathname, sizeof(struct db_key), &db_key_cmp,
-                     NULL, 0);
+                     NULL, ro ? DB_HL_RDONLY : 0);
     if (ret != 0)
         return ret;
 
@@ -274,8 +521,9 @@ main(int argc, char **argv)
     const char *db_pathname = DB_PATHNAME;
     enum op op = 0;
     int ret;
+    int ro = 1;
 
-    ret = parse_cmdline(argc, argv, &op, &db_pathname);
+    ret = parse_cmdline(argc, argv, &op, &db_pathname, &ro);
     if (ret != 0)
         return (ret == -2) ? EXIT_SUCCESS : EXIT_FAILURE;
     if (op == 0) {
@@ -285,7 +533,7 @@ main(int argc, char **argv)
 
     switch (op) {
     case OP_FIX_UP:
-        ret = do_fix_up_interactive(db_pathname);
+        ret = do_fix_up_interactive(db_pathname, ro);
         break;
     default:
         ret = -EIO;
