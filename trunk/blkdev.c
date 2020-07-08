59182cf550a3a800f475f25e93a4ed6da7593563
Richard Bromley <bromleyrh@gmail.com>
2020-07-07 22:07:58 -0700

    Commit initial work in blkdev (3)
    Commit UUID: d4a89848-89d3-463c-8d68-56d78416fde7

--- trunk/blkdev.c
+++ trunk/blkdev.c
@@ -19,14 +19,23 @@
 
 #include <errno.h>
 #include <fcntl.h>
+#include <inttypes.h>
 #include <limits.h>
 #include <stdarg.h>
 #include <stddef.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 
+#ifdef HAVE_LINUX_FS_H
+#include <linux/fs.h>
+#else
+#error "Support for platform not yet implemented"
+#endif
+
 #include <sys/file.h>
+#include <sys/ioctl.h>
 #include <sys/mman.h>
 #include <sys/param.h>
 #include <sys/stat.h>
@@ -35,6 +44,21 @@
 #include <sys/vfs.h>
 #endif
 
+#define MAGIC 0
+
+struct disk_header {
+    uint32_t    magic;
+    uint64_t    off;        /* data area offset */
+    uint64_t    joff;       /* journal offset */
+    uint64_t    blkdevsz;   /* total device size */
+    uint8_t     padding[4096 - sizeof(uint32_t) - 3 * sizeof(uint64_t)];
+} __attribute__((packed));
+
+/* The disk header size must be a multiple of 4096 */
+STATIC_ASSERT(sizeof(struct disk_header) == 4096);
+
+#define BLKDEV_OPEN(bctx) ((bctx)->hdr.blkdevsz > 0)
+
 #define BCTX_FD(bctx, n) ((bctx)->fd[n])
 
 #define DFD(bctx) BCTX_FD(bctx, 0)
@@ -42,9 +66,10 @@
 #define JFD(bctx) BCTX_FD(bctx, 2)
 
 struct blkdev_ctx {
-    int     fd[3];
-    off_t   off;
-    off_t   joff;
+    struct disk_header  hdr;
+    int                 fd[3];
+    int                 init;
+    int                 jinit;
 };
 
 struct fs_ops {
@@ -109,9 +134,18 @@ static int fs_blkdev_fstatfs(void *ctx, int fd, struct statfs *buf);
 
 #define JOURNAL_FILE_SUFFIX "_journal"
 
+/* These sizes must be a multiple of 4096 */
+#define DATA_MIN_SIZE (2 * 1024 * 1024)
+#define JOURNAL_SIZE (256 * 1024 * 1024)
+
+#define BLKDEV_MIN_SIZE \
+    (sizeof(struct disk_header) + DATA_MIN_SIZE + JOURNAL_SIZE)
+
 #if defined(HAVE_LINUX_MAGIC_H) && !defined(NFS_SUPER_MAGIC)
 #define NFS_SUPER_MAGIC 1
 #endif
+
+#define IO_SIZE 4096
 
 const struct fs_ops fs_blkdev_ops = {
     .openfs         = &fs_blkdev_openfs,
@@ -156,6 +190,17 @@ static int do_pfdatasync(int, const struct interrupt_data *);
 
 /*static int falloc(int, off_t, off_t);
 */
+static int blkdev_flags(int);
+static int get_blkdev_size(int, uint64_t *);
+
+static void init_header(struct disk_header *, uint64_t);
+static int read_header(int, struct disk_header *);
+static int write_header(int, const struct disk_header *);
+
+static int open_blkdev(int, int, int, int *, struct blkdev_ctx *);
+
+static int check_fd_regular(int, struct blkdev_ctx *);
+
 static int
 err_to_errno(int err)
 {
@@ -307,6 +352,100 @@ falloc(int fd, off_t offset, off_t len)
 #endif
 
 static int
+blkdev_flags(int flags)
+{
+    return (flags & ~(O_CREAT | O_EXCL));
+}
+
+static int
+get_blkdev_size(int fd, uint64_t *sz)
+{
+    uint64_t res;
+
+    if (ioctl(fd, BLKGETSIZE64, &res) == -1)
+        return -errno;
+
+    *sz = (uint64_t)res;
+    return 0;
+}
+
+static void
+init_header(struct disk_header *hdr, uint64_t blkdevsz)
+{
+    hdr->off = sizeof(*hdr);
+    hdr->joff = (blkdevsz - JOURNAL_SIZE) / 4096 * 4096;
+    hdr->blkdevsz = blkdevsz;
+}
+
+static int
+read_header(int fd, struct disk_header *hdr)
+{
+    return (do_ppread(fd, hdr, sizeof(*hdr), 0, IO_SIZE, NULL) == sizeof(*hdr))
+           ? 0 : -EIO;
+}
+
+static int
+write_header(int fd, const struct disk_header *hdr)
+{
+    return (do_ppwrite(fd, hdr, sizeof(*hdr), 0, IO_SIZE, NULL) == sizeof(*hdr))
+           ? 0 : -EIO;
+}
+
+static int
+open_blkdev(int fd, int create, int ro, int *initialized,
+            struct blkdev_ctx *bctx)
+{
+    int err;
+    uint64_t blkdevsz;
+
+    err = get_blkdev_size(fd, &blkdevsz);
+    if (err)
+        return err;
+    if (blkdevsz < BLKDEV_MIN_SIZE)
+        return -ERANGE;
+
+    err = read_header(fd, &bctx->hdr);
+    if (err)
+        return err;
+    if (bctx->hdr.magic != MAGIC) { /* not formatted */
+        fputs("Device not formatted\n", stderr);
+        return -EILSEQ;
+    }
+    if (bctx->hdr.blkdevsz == 0) { /* not initialized */
+        if (ro)
+            return -EROFS;
+        if (!create)
+            return -ENOENT;
+        init_header(&bctx->hdr, blkdevsz);
+        err = write_header(fd, &bctx->hdr);
+        if (err)
+            return err;
+        if (fsync(fd) == -1)
+            return -errno;
+        *initialized = 1;
+        return 0;
+    }
+    if (bctx->hdr.blkdevsz != blkdevsz) {
+        /* device size changed after initialization */
+        fprintf(stderr, "Device size changed from %" PRIu64 "\n",
+                bctx->hdr.blkdevsz);
+        return -EILSEQ;
+    }
+
+    *initialized = 0;
+    return 0;
+}
+
+static int
+check_fd_regular(int fd, struct blkdev_ctx *bctx)
+{
+    if ((fd != FD(bctx)) && (fd != JFD(bctx)))
+        return err_to_errno((fd == DFD(bctx)) ? EINVAL : EBADF);
+
+    return 0;
+}
+
+static int
 fs_blkdev_openfs(void **ctx, void *args)
 {
     size_t i;
@@ -318,9 +457,11 @@ fs_blkdev_openfs(void **ctx, void *args)
     if (ret == NULL)
         return -errno;
 
+    ret->hdr.blkdevsz = 0;
+
     for (i = 0; i < ARRAY_SIZE(ret->fd); i++)
         ret->fd[i] = -1;
-    ret->off = -1;
+    ret->init = ret->jinit = -1;
 
     *ctx = ret;
     return 0;
@@ -336,19 +477,20 @@ fs_blkdev_closefs(void *ctx)
 
 /*
  * This function receives open requests for exactly the following pathnames:
- * - ".", whose corresponding file should have the directory type
- * - "x_journal" for some prefix "x", whose corresponding file must have the
+ * - exactly one of "." and "x/" (with the O_DIRECTORY flag set), whose
+ *   corresponding file should have the directory type
+ * - "y_journal" for some prefix "y", whose corresponding file must have the
  *   regular file type
- * - "x" for the same string "x" as the file above, whose corresponding file
+ * - "y" for the same string "y" as the file above, whose corresponding file
  *   must also have the regular file type
  *
  * The function fs_blkdev_openat() should resolve the above pathnames such that
  * each refers to a distinct file or a nonexistent file. In particular, it is
- * important that x_journal and x resolve to different files if they both refer
+ * important that y_journal and y resolve to different files if they both refer
  * to existing files. If this is not guaranteed, corruption may occur.
  *
  * After a file system is formatted but before it is first mounted, neither the
- * file x_journal nor x should exist. In this case, fs_blkdev_openat() should
+ * file y_journal nor y should exist. In this case, fs_blkdev_openat() should
  * return descriptors for these files only if O_CREAT is specified in the flags
  * argument, in which case it should create a new file for each. The results for
  * the case where a file system has not been formatted are undefined.
@@ -366,14 +508,18 @@ fs_blkdev_openat(void *ctx, int dfd, const char *pathname, int flags,
 {
     char buf[PATH_MAX];
     int *bfd;
-    int ret;
+    int create, ro;
+    int dir;
+    int *init, initialized;
+    int res, ret;
     struct blkdev_ctx *bctx = (struct blkdev_ctx *)ctx;
 
     (void)ap;
 
-    if (strcmp(".", pathname) == 0)
+    if (flags & O_DIRECTORY) {
         bfd = &DFD(bctx);
-    else {
+        dir = 1;
+    } else {
         size_t plen, slen;
 
         slen = strlen(pathname);
@@ -388,13 +534,48 @@ fs_blkdev_openat(void *ctx, int dfd, const char *pathname, int flags,
             bfd = &JFD(bctx);
         } else
             bfd = &FD(bctx);
+
+        dir = 0;
     }
 
-    ret = openat(dfd, pathname, flags);
-    if (ret != -1)
-        *bfd = ret;
+    ret = openat(dfd, pathname, dir ? flags : blkdev_flags(flags));
+    if (ret == -1)
+        return -1;
 
+    if (dir)
+        goto end;
+
+    create = !!(flags & O_CREAT);
+    ro = (flags & O_ACCMODE) == O_RDONLY;
+
+    if (!BLKDEV_OPEN(bctx)) {
+        res = open_blkdev(ret, create, ro, &initialized, bctx);
+        if (res != 0)
+            goto err;
+        bctx->init = bctx->jinit = !initialized;
+    }
+
+    init = (bfd == &FD(bctx)) ? &bctx->init : &bctx->jinit;
+
+    if (create) {
+        if (!(*init))
+            *init = 1;
+        else if (flags & O_EXCL) {
+            res = -EEXIST;
+            goto err;
+        }
+    } else if (!(*init)) {
+        res = -ENOENT;
+        goto err;
+    }
+
+end:
+    *bfd = ret;
     return ret;
+
+err:
+    close(ret);
+    return err_to_errno(-res);
 }
 
 static int
@@ -456,14 +637,25 @@ fs_blkdev_mprotect(void *ctx, void *addr, size_t len, int prot)
     return mprotect(addr, len, prot);
 }
 
+/*
+ * This function is only required to set the st_mode and st_size fields of *s
+ * appropriately, and is only required to support regular files.
+ */
 static int
 fs_blkdev_fstat(void *ctx, int fd, struct stat *s)
 {
-    (void)ctx;
+    int ret;
+    struct blkdev_ctx *bctx = (struct blkdev_ctx *)ctx;
 
-    return err_to_errno(ENOSYS);
+    ret = check_fd_regular(fd, bctx);
+    if (ret != 0)
+        return ret;
 
-    return fstat(fd, s);
+    memset(s, 0, sizeof(*s));
+    s->st_mode = S_IFREG;
+    s->st_size = (fd == FD(bctx)) ? bctx->hdr.blkdevsz : JOURNAL_SIZE;
+
+    return 0;
 }
 
 static size_t
@@ -541,8 +733,10 @@ static int
 fs_blkdev_flock(void *ctx, int fd, int operation)
 {
     (void)ctx;
+    (void)fd;
+    (void)operation;
 
-    return flock(fd, operation);
+    return 0;
 }
 
 static int
